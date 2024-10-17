0e389c5d4b9f6c45a477eb0524512743b77a37e1
SaraBarrett <sara.c.mon@gmail.com>
2024-10-17 12:47:56 +0100

    controller w/data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,10 +7,22 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function returnViewAllUsers(){
-        return view('users.all_users');
+        $cesaeInfo = $this->getCesaeInfo();
+
+        return view('users.all_users', compact('cesaeInfo'));
     }
 
     public function returnViewAddUser(){
         return view('users.add_user');
+    }
+
+    private function getCesaeInfo(){
+        $cesaeInfo = [
+            'name' => 'Cesae',
+            'address' => 'Rua Ciríaco Cardoso 186, 4150-212 Porto',
+            'email' => 'cesae@cesae.pt'
+            ];
+
+       return $cesaeInfo;
     }
 }
--- resources/views/users/all_users.blade.php
+++ resources/views/users/all_users.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.femaster')
 
 @section('content')
+    <h6>Info do Cesae</h6>
+    <ul>
+        <li>{{ $cesaeInfo['name'] }}</li>
+        <li>{{ $cesaeInfo['address'] }}</li>
+        <li>{{ $cesaeInfo['email'] }}</li>
+    </ul>
     <h3>Sou uma blade para todos os users </h3>
 @endsection
